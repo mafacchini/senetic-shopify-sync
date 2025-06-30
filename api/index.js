9e1466ef9cbf3ef089073666d63332010dd26da0
Massimo Facchini <massimo@Massimos-MacBook-Pro.local>
2025-06-30 23:51:28 +0200

    🧹 Clean project: remove unused local dev files

--- api/index.js
+++ api/index.js
@@ -86,11 +86,7 @@ app.get('/import-shopify', async (req, res) => {
       const shopifyProduct = {
         product: {
           title: prodotto.itemDescription || '',
-          body_html: prodotto.longItemDescription ? 
-            he.decode(prodotto.longItemDescription)
-              .replace(/href="\/\/senetic\.pl\//g, 'href="https://senetic.pl/')
-              .replace(/src="\/\/senetic\.pl\//g, 'src="https://senetic.pl/')
-            : '',
+          body_html: prodotto.longItemDescription ? he.decode(prodotto.longItemDescription) : '',
           vendor: prodotto.productPrimaryBrand?.brandNodeName || '',
           product_type: prodotto.productSecondaryCategory?.categoryNodeName || '',
           variants: [
@@ -261,11 +257,7 @@ app.get('/import-shopify-cron', async (req, res) => {
       const shopifyProduct = {
         product: {
           title: prodotto.itemDescription || '',
-          body_html: prodotto.longItemDescription ? 
-            he.decode(prodotto.longItemDescription)
-              .replace(/href="\/\/senetic\.pl\//g, 'href="https://senetic.pl/')
-              .replace(/src="\/\/senetic\.pl\//g, 'src="https://senetic.pl/')
-            : '',
+          body_html: prodotto.longItemDescription ? he.decode(prodotto.longItemDescription) : '',
           vendor: prodotto.productPrimaryBrand?.brandNodeName || '',
           product_type: prodotto.productSecondaryCategory?.categoryNodeName || '',
           variants: [
